b068abbd05f2d1481482db614c99cb5a0a316c1c
jackmonaco20 <jackmonaco20@yahoo.com>
2023-08-06 15:14:18 -0400

    removed astro compress from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
    site: 'https://unwrapped.design',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+  integrations: [tailwind(), image(),  partytown(), sitemap()]
 });
